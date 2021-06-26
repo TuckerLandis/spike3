b69c8332c9cbfa1fd97680f8dc8601f50ae4e1cb
TuckerLandis <landistuckerc@gmail.com>
2021-06-26 17:12:06 -0500

    scale abstracto

--- src/Sequencer.js
+++ src/Sequencer.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import * as Tone from 'tone'
 
-function Sequencer () {
+function Sequencer() {
     // states for playing boolean and for playbutton text. these get flipped when pressing play or stop
     const [isPlaying, setIsPlaying] = useState(false)
     const [playButtonText, setPlayButtonText] = useState('play')
@@ -12,7 +12,7 @@ function Sequencer () {
     // state for scale. changed on handleScaleChange
     const [scale, setScale] = useState(c_major)
 
-    
+
 
     // state for each step in sequence
     const [noteOne, setNoteOne] = useState(null)
@@ -32,64 +32,67 @@ function Sequencer () {
     let notes = [noteOne, noteTwo, noteThree, noteFour, noteFive, noteSix, noteSeven, noteEight]
 
     // these handle the input -> state change of each note select below
-   const handleSelectOne = (event) => {
-    setNoteOne(event.target.value)
-   }
-   const handleSelectTwo = (event) => {
-    setNoteTwo(event.target.value)
-   }
-   const handleSelectThree = (event) => {
-    setNoteThree(event.target.value)
-   }
-   const handleSelectFour = (event) => {
-    setNoteFour(event.target.value)
-   }
-   const handleSelectFive = (event) => {
-    setNoteFive(event.target.value)
-   }
-   const handleSelectSix = (event) => {
-    setNoteSix(event.target.value)
-   }
-   const handleSelectSeven = (event) => {
-    setNoteSeven(event.target.value)
-   }
-   const handleSelectEight = (event) => {
-    setNoteEight(event.target.value)
-   }
-
-
-
-
-
+    const handleSelectOne = (event) => {
+        setNoteOne(event.target.value)
+    }
+    const handleSelectTwo = (event) => {
+        setNoteTwo(event.target.value)
+    }
+    const handleSelectThree = (event) => {
+        setNoteThree(event.target.value)
+    }
+    const handleSelectFour = (event) => {
+        setNoteFour(event.target.value)
+    }
+    const handleSelectFive = (event) => {
+        setNoteFive(event.target.value)
+    }
+    const handleSelectSix = (event) => {
+        setNoteSix(event.target.value)
+    }
+    const handleSelectSeven = (event) => {
+        setNoteSeven(event.target.value)
+    }
+    const handleSelectEight = (event) => {
+        setNoteEight(event.target.value)
+    }
 
     const playButton = () => {
 
         console.log(notes);
-        
+
+        if (!isPlaying) {  // starts Tone if stopped
+            setIsPlaying(true)  // flips playing boolean
+            setPlayButtonText('stop') // flips button text
+
+            Tone.start() // start tone audio context on user interaction per spec of web audio api
 
 
-        if (!isPlaying) {
-            setIsPlaying(true)
-            setPlayButtonText('stop')
-        Tone.start()
 
-        const seq = new Tone.Sequence((time, note) => {
-            synth.triggerAttackRelease(note, 0.1, time);
-            // subdivisions are given as subarrays
-        }, notes).start(0);
-        Tone.Transport.start();
+            // leaving space mentally for a section for configuring the BPM and (stretch) probability of the sequence
 
 
-        } else {
-            setIsPlaying(false)
-            setPlayButtonText('play')
-            Tone.Transport.stop();
+
+
+            const seq = new Tone.Sequence((time, note) => { // instantiates sequence of triggers on synth
+                synth.triggerAttackRelease(note, 0.1, time); // note comes from notes array state. 
+                // subdivisions are given as subarrays
+            }, notes).start(0); // which notes? notes array. start takes arg of "now" time
+
+            // starts the transport. what actually STARTs our sequence
+            Tone.Transport.start();
+
+
+        } else { // if transport is playing, 
+            setIsPlaying(false) // set play bool to off
+            setPlayButtonText('play') // flip play button text
+            Tone.Transport.stop(); // STOP the transport
         }
-        
+
 
     }
-    
-    
+
+
 
 
 
@@ -100,149 +103,79 @@ function Sequencer () {
         <div>
             <button onClick={playButton}>{playButtonText}</button>
             <div>
-            <select name="selectOne" id="note-select-1" onChange={handleSelectOne}>
-                   
 
-                   {/* uses scale state to return a list of notes in selected scale */}
-                   {scale.map((note, i) => {
-                       return (
-                           <option key={i} value={note}>{note}</option>
-                       )
-                   })}
-                   
-                   
-                   
-                   
-                   
-                   
-                    {/* <option value="off">off</option>
-                    <option value="A4">A4</option>
-                    <option value="Bb4">Bb4</option>
-                    <option value="B4">B4</option>
-                    <option value="C4">C4</option>
-                    <option value="C#4">C#4</option>
-                    <option value="D4">D4</option>
-                    <option value="D#4">D#4</option>
-                    <option value="E4">E4</option>
-                    <option value="F4">F4</option>
-                    <option value="F#4">F#4</option>
-                    <option value="G4">G4</option>
-                    <option value="Ab4">Ab4</option> */}
+                <select name="selectOne" id="note-select-1" onChange={handleSelectOne}>
+                    {/* uses scale state to return a list of notes in selected scale */}
+                    {scale.map((note, i) => {
+                        return (
+                            <option key={i} value={note}>{note}</option>
+                        )
+                    })}
                 </select>
 
                 <select name="selectTwo" id="note-select-2" onChange={handleSelectTwo}>
-                    <option value="off">off</option>
-                    <option value="A4">A4</option>
-                    <option value="Bb4">Bb4</option>
-                    <option value="B4">B4</option>
-                    <option value="C4">C4</option>
-                    <option value="C#4">C#4</option>
-                    <option value="D4">D4</option>
-                    <option value="D#4">D#4</option>
-                    <option value="E4">E4</option>
-                    <option value="F4">F4</option>
-                    <option value="F#4">F#4</option>
-                    <option value="G4">G4</option>
-                    <option value="Ab4">Ab4</option>
+                    {/* uses scale state to return a list of notes in selected scale */}
+                    {scale.map((note, i) => {
+                        return (
+                            <option key={i} value={note}>{note}</option>
+                        )
+                    })}
                 </select>
 
                 <select name="selectThree" id="note-select-3" onChange={handleSelectThree}>
-                    <option value="off">off</option>
-                    <option value="A4">A4</option>
-                    <option value="Bb4">Bb4</option>
-                    <option value="B4">B4</option>
-                    <option value="C4">C4</option>
-                    <option value="C#4">C#4</option>
-                    <option value="D4">D4</option>
-                    <option value="D#4">D#4</option>
-                    <option value="E4">E4</option>
-                    <option value="F4">F4</option>
-                    <option value="F#4">F#4</option>
-                    <option value="G4">G4</option>
-                    <option value="Ab4">Ab4</option>
+                    {/* uses scale state to return a list of notes in selected scale */}
+                    {scale.map((note, i) => {
+                        return (
+                            <option key={i} value={note}>{note}</option>
+                        )
+                    })}
                 </select>
 
                 <select name="selectFour" id="note-select-4" onChange={handleSelectFour}>
-                    <option value="off">off</option>
-                    <option value="A4">A4</option>
-                    <option value="Bb4">Bb4</option>
-                    <option value="B4">B4</option>
-                    <option value="C4">C4</option>
-                    <option value="C#4">C#4</option>
-                    <option value="D4">D4</option>
-                    <option value="D#4">D#4</option>
-                    <option value="E4">E4</option>
-                    <option value="F4">F4</option>
-                    <option value="F#4">F#4</option>
-                    <option value="G4">G4</option>
-                    <option value="Ab4">Ab4</option>
+                    {/* uses scale state to return a list of notes in selected scale */}
+                    {scale.map((note, i) => {
+                        return (
+                            <option key={i} value={note}>{note}</option>
+                        )
+                    })}
                 </select>
 
                 <select name="selectFive" id="note-select-5" onChange={handleSelectFive}>
-                    <option value="off">off</option>
-                    <option value="A4">A4</option>
-                    <option value="Bb4">Bb4</option>
-                    <option value="B4">B4</option>
-                    <option value="C4">C4</option>
-                    <option value="C#4">C#4</option>
-                    <option value="D4">D4</option>
-                    <option value="D#4">D#4</option>
-                    <option value="E4">E4</option>
-                    <option value="F4">F4</option>
-                    <option value="F#4">F#4</option>
-                    <option value="G4">G4</option>
-                    <option value="Ab4">Ab4</option>
+                    {/* uses scale state to return a list of notes in selected scale */}
+                    {scale.map((note, i) => {
+                        return (
+                            <option key={i} value={note}>{note}</option>
+                        )
+                    })}
                 </select>
 
                 <select name="selectSix" id="note-select-6" onChange={handleSelectSix}>
-                    <option value="off">off</option>
-                    <option value="A4">A4</option>
-                    <option value="Bb4">Bb4</option>
-                    <option value="B4">B4</option>
-                    <option value="C4">C4</option>
-                    <option value="C#4">C#4</option>
-                    <option value="D4">D4</option>
-                    <option value="D#4">D#4</option>
-                    <option value="E4">E4</option>
-                    <option value="F4">F4</option>
-                    <option value="F#4">F#4</option>
-                    <option value="G4">G4</option>
-                    <option value="Ab4">Ab4</option>
+                    {/* uses scale state to return a list of notes in selected scale */}
+                    {scale.map((note, i) => {
+                        return (
+                            <option key={i} value={note}>{note}</option>
+                        )
+                    })}
                 </select>
 
                 <select name="selectSeven" id="note-select-7" onChange={handleSelectSeven}>
-                    <option value="off">off</option>
-                    <option value="A4">A4</option>
-                    <option value="Bb4">Bb4</option>
-                    <option value="B4">B4</option>
-                    <option value="C4">C4</option>
-                    <option value="C#4">C#4</option>
-                    <option value="D4">D4</option>
-                    <option value="D#4">D#4</option>
-                    <option value="E4">E4</option>
-                    <option value="F4">F4</option>
-                    <option value="F#4">F#4</option>
-                    <option value="G4">G4</option>
-                    <option value="Ab4">Ab4</option>
+                    {/* uses scale state to return a list of notes in selected scale */}
+                    {scale.map((note, i) => {
+                        return (
+                            <option key={i} value={note}>{note}</option>
+                        )
+                    })}
                 </select>
 
                 <select name="selectEight" id="note-select-8" onChange={handleSelectEight}>
-                    <option value="off">off</option>
-                    <option value="A4">A4</option>
-                    <option value="Bb4">Bb4</option>
-                    <option value="B4">B4</option>
-                    <option value="C4">C4</option>
-                    <option value="C#4">C#4</option>
-                    <option value="D4">D4</option>
-                    <option value="D#4">D#4</option>
-                    <option value="E4">E4</option>
-                    <option value="F4">F4</option>
-                    <option value="F#4">F#4</option>
-                    <option value="G4">G4</option>
-                    <option value="Ab4">Ab4</option>
+                    {/* uses scale state to return a list of notes in selected scale */}
+                    {scale.map((note, i) => {
+                        return (
+                            <option key={i} value={note}>{note}</option>
+                        )
+                    })}
                 </select>
             </div>
-                
 
         </div>
     )
@@ -250,17 +183,3 @@ function Sequencer () {
 }
 
 export default Sequencer
-
-// A	440
-// B flat	466
-// B	494
-// C	523
-// C sharp	554
-// D	587
-// D sharp	622
-// E	659
-// F	698
-// F sharp	740
-// G	784
-// A flat	831
-// A
